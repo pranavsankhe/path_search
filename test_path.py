9960e7fc44f548eb26b5a936dec968ab5cbbfb85
Pranav Sankhe <pranavsankhe@Pranavs-MacBook-Pro.local>
2018-04-15 16:14:33 -0400

    produces the right ouput but could be implemented better

--- test_path.py
+++ test_path.py
@@ -1,92 +1,93 @@
 # -----------
 # User Instructions:
-# 
-# Modify the function search so that it returns
-# a table of values called expand. This table
-# will keep track of which step each node was
-# expanded.
 #
-# Make sure that the initial cell in the grid 
-# you return has the value 0.
+# Modify the the search function so that it becomes
+# an A* search algorithm as defined in the previous
+# lectures.
+#
+# Your function should return the expanded grid
+# which shows, for each element, the count when
+# it was expanded or -1 if the element was never expanded.
+# 
+# If there is no path from init to goal,
+# the function should return the string 'fail'
 # ----------
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0]]
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
+
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
 
-delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
-def search(grid,init,goal,cost):
+def search(grid,init,goal,cost,heuristic):
     # ----------------------------------------
-    # modify code below
+    # modify the code below
     # ----------------------------------------
-    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
     closed[init[0]][init[1]] = 1
-    
-    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
-    action = [[-1 for col in range(len(grid[0]))]  for row in range(len(grid))]
-    policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+
+    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+
     x = init[0]
     y = init[1]
     g = 0
 
     open = [[g, x, y]]
+
     found = False  # flag that is set when search is complete
     resign = False # flag set if we can't find expand
     count = 0
-
+    
     while not found and not resign:
         if len(open) == 0:
             resign = True
+            return "Fail"
         else:
             open.sort()
             open.reverse()
             next = open.pop()
-            
-        
             x = next[1]
             y = next[2]
             g = next[0]
             expand[x][y] = count
             count += 1
+            
             if x == goal[0] and y == goal[1]:
                 found = True
             else:
+                candidates = []
                 for i in range(len(delta)):
                     x2 = x + delta[i][0]
                     y2 = y + delta[i][1]
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
                             g2 = g + cost
-                            open.append([g2, x2, y2])
-                            closed[x2][y2] = 1
-                            # for visulisation: action[x2][y2] = delta_name[i]
-                            action[x2][y2] = i
+                            candidates.append([ (heuristic[x2][y2] + g2), x2, y2, g2]) 
+                            compare = min(candidates)
+
+                for candidate in (candidates):           
+                    if candidate[0] == compare[0]:
+                        open.append([candidate[3], candidate[1], candidate[2]])
+                        closed[candidate[1]][candidate[2]] = 1
 
 
-    x = goal[0]
-    y = goal[1]
-    policy[x][y] = '*'
-    while x != init[0] or y != init[1]:
-        x2 = x - delta[action[x][y]][0]
-        y2 = y - delta[action[x][y]][1]
-        policy[x2][y2] = delta_name[action[x][y]]
-        x = x2
-        y = y2
-
-    for row in policy:
-        print(row)
-        
     return expand
 
-print (search(grid,init,goal,cost))
+print(search(grid,init,goal,cost,heuristic))
