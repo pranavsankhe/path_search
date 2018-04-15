168e86fd86baa6b8cef1c9588211d5f1f1928cbf
Pranav Sankhe <pranavsankhe@Pranavs-MacBook-Pro.local>
2018-04-15 19:45:08 -0400

    dynamic programming approch to robotic path finding

--- test_path.py
+++ test_path.py
@@ -1,32 +1,27 @@
-# -----------
+# ----------
 # User Instructions:
-#
-# Modify the the search function so that it becomes
-# an A* search algorithm as defined in the previous
-# lectures.
-#
-# Your function should return the expanded grid
-# which shows, for each element, the count when
-# it was expanded or -1 if the element was never expanded.
 # 
-# If there is no path from init to goal,
-# the function should return the string 'fail'
+# Write a function optimum_policy that returns
+# a grid which shows the optimum policy for robot
+# motion. This means there should be an optimum
+# direction associated with each navigable cell from
+# which the goal can be reached.
+# 
+# Unnavigable cells as well as cells from which 
+# the goal cannot be reached should have a string 
+# containing a single space (' '), as shown in the 
+# previous video. The goal cell should have '*'.
 # ----------
 
 grid = [[0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0]]
-heuristic = [[9, 8, 7, 6, 5, 4],
-             [8, 7, 6, 5, 4, 3],
-             [7, 6, 5, 4, 3, 2],
-             [6, 5, 4, 3, 2, 1],
-             [5, 4, 3, 2, 1, 0]]
-
+        [0, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 1, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
-cost = 1
+cost = 1 # the cost associated with moving from a cell to an adjacent one
+policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 
 delta = [[-1, 0 ], # go up
          [ 0, -1], # go left
@@ -35,57 +30,37 @@ delta = [[-1, 0 ], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-def search(grid,init,goal,cost,heuristic):
+def optimum_policy(grid,goal,cost):
     # ----------------------------------------
-    # modify the code below
+    # modify code below
     # ----------------------------------------
-    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-    closed[init[0]][init[1]] = 1
+    value = [[99 for row in range(len(grid[0]))] for col in range(len(grid))]
+    change = True
 
-    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
-    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    while change:
+        change = False
 
-    x = init[0]
-    y = init[1]
-    g = 0
-    h = heuristic[x][y] + g
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if goal[0] == x and goal[1] == y:
+                    if value[x][y] > 0:
+                        value[x][y] = 0
+                        policy[x][y] = '*'
+                        change = True
 
-    open = [[h, g, x, y]]
+                elif grid[x][y] == 0:
+                    for a in range(len(delta)):
+                        x2 = x + delta[a][0]
+                        y2 = y + delta[a][1]
 
-    found = False  # flag that is set when search is complete
-    resign = False # flag set if we can't find expand
-    count = 0
-    
-    while not found and not resign:
-        if len(open) == 0:
-            resign = True
-            return "Fail"
-        else:
-            open.sort()
-            open.reverse()
-            next = open.pop()
-            x = next[2]
-            y = next[3]
-            g = next[1]
+                        if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
+                            v2 = value[x2][y2] + cost
 
-            expand[x][y] = count
-            count += 1
-            
-            if x == goal[0] and y == goal[1]:
-                found = True
-            else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            h2 = heuristic[x2][y2] + g
-                            open.append([h2, g2, x2, y2])
-                            closed[x2][y2] = 1
+                            if v2 < value[x][y]:
+                                change = True
+                                value[x][y] = v2
+                                policy[x][y] = delta_name[a] 
 
-
-
-    return expand
-
-print(search(grid,init,goal,cost,heuristic))
+    return policy
+for i  in (optimum_policy(grid,goal,cost)):
+    print(i)
